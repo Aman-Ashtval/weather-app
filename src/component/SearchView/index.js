9d401f1352eca7153db022c76de5cbcb4a9fb10f
Aman Kumar <9k725304@gmail.com>
2024-06-16 11:30:00 +0530

    implement user inputs

--- src/component/SearchView/index.js
+++ src/component/SearchView/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 
 import AppContext from "../../context/AppContext";
@@ -5,6 +6,21 @@ import AppContext from "../../context/AppContext";
 import './index.css'
 
 const SearchView = props => {
+    const{changeCityName} = props
+
+    const[cityName, setCityName] = useState('');
+
+    const onChangeCityName = event => setCityName(event.target.value)
+
+    // key enter event
+    const onEnter = event => {
+        if(event.key === 'Enter'){
+            changeCityName(cityName);
+        }
+    }
+
+    // onclick event
+    const onSearchCity = () => changeCityName(cityName);
 
     return (
         <AppContext.Consumer>
@@ -19,8 +35,8 @@ const SearchView = props => {
                                     <h3 className={`welcome ${isDark ? 'text-light': 'text-secondary'}`}>Welcome!</h3>
                                     <p className={`discription-p ${isDark ? 'text-light': 'text-secondary'}`}>search weather by city name</p>
                                     <div className="d-flex align-items-center w-100 mt-5">
-                                        <input type="search" placeholder="enter city name..." className="search-input" />
-                                        <button type="button" className="search-btn">
+                                        <input type="search" placeholder="enter city name..." className="search-input" onChange={onChangeCityName} onKeyDown={onEnter} />
+                                        <button type="button" className="search-btn" onClick={onSearchCity}>
                                             <FiSearch />
                                         </button>
                                     </div>
